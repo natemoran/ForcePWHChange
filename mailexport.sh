0eaa5519ee47deb1091bd0e71d1077850dc449ad
Nate Moran <nate.moran@northeastern.edu>
2021-09-27 15:15:14 -0400

    mail export added

---
+++ mailexport.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+export $(xargs <.env)
+
+echo "Building parents Email file"
+java -jar unboundid-ldapsdk-6.0.1.jar ldapsearch \
+    --hostname $LDAPHOST \
+    --port $LDAPPORT  \
+    --useSSL \
+    --bindDN "cn=directory manager" \
+    --bindPassword $LDAPPWD \
+    --baseDN "ou=people,dc=neu,dc=edu" \
+    --scope sub  \
+    --outputFormat multi-valued-csv \
+    --outputFile parents.csv \
+    --requestedAttribute givenname \
+    --requestedAttribute sn \
+    --requestedAttribute neuedualtmail \
+    --terse \
+    --filter "(&(pager=nopwchg)(neuedualtmail=*)(eduPersonAffiliation=parent))" 
+wc parents.csv
+
+echo "Building students Email file"
+java -jar unboundid-ldapsdk-6.0.1.jar ldapsearch \
+    --hostname $LDAPHOST \
+    --port $LDAPPORT  \
+    --useSSL \
+    --bindDN "cn=directory manager" \
+    --bindPassword $LDAPPWD \
+    --baseDN "ou=student,dc=neu,dc=edu" \
+    --scope sub  \
+    --outputFormat multi-valued-csv \
+    --outputFile students.csv \
+    --requestedAttribute givenname \
+    --requestedAttribute sn \
+    --requestedAttribute mail \
+    --terse \
+    --filter "(&(|(eduPersonAffiliation=student)(eduPersonAffiliation=ugadmit))(pager=nopwchg)(mail=*))" 
+wc students.csv    
+
+echo "Building facultyandstaff Email file"
+java -jar unboundid-ldapsdk-6.0.1.jar ldapsearch \
+    --hostname $LDAPHOST \
+    --port $LDAPPORT  \
+    --useSSL \
+    --bindDN "cn=directory manager" \
+    --bindPassword $LDAPPWD \
+    --baseDN "ou=facultyandstaff,dc=neu,dc=edu" \
+    --scope sub  \
+    --outputFormat multi-valued-csv \
+    --outputFile facultyandstaff.csv \
+    --requestedAttribute givenname \
+    --requestedAttribute sn \
+    --requestedAttribute mail \
+    --terse \
+    --filter "(&(pager=nopwchg)(mail=*))"    
+wc facultyandstaff.csv      
+
+echo "Building alumni Email file"
+java -jar unboundid-ldapsdk-6.0.1.jar ldapsearch \
+    --hostname $LDAPHOST \
+    --port $LDAPPORT  \
+    --useSSL \
+    --bindDN "cn=directory manager" \
+    --bindPassword $LDAPPWD \
+    --baseDN "ou=student,dc=neu,dc=edu" \
+    --scope sub  \
+    --outputFormat multi-valued-csv \
+    --outputFile alumni.csv \
+    --requestedAttribute givenname \
+    --requestedAttribute sn \
+    --requestedAttribute mail \
+    --terse \
+    --filter "(&(eduPersonAffiliation=alumni)(pager=nopwchg)(mail=*)(!(|(eduPersonAffiliation=student))))" 
+wc alumni.csv     
